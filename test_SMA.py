8e466a5a12bbe46937b9d6f45de5ad3bf35f437d
cgit6 <109117801+cgit6@users.noreply.github.com>
2023-04-19 00:44:03 +0800

    2023/4/19

--- test_SMA.py
+++ test_SMA.py
@@ -2,10 +2,12 @@ import numpy as np
 import copy as copy
 
 def initialization(pop,ub,lb,dim):
-    X = np.zeros([pop,dim]) #声明空间
+    #声明空间
+    X = np.zeros([pop,dim]) 
     for i in range(pop):
         for j in range(dim):
-            X[i,j]=(ub[j]-lb[j])*np.random.random()+lb[j] #生成[lb,ub]之间的随机数
+            #生成[lb,ub]之间的随机数
+            X[i,j]=(ub[j]-lb[j])*np.random.random()+lb[j] 
     
     return X
      
@@ -39,94 +41,65 @@ def SortPosition(X,index):
     return Xnew
 
 
-def ESMA(pop,dim,lb,ub,MaxIter,fun):
-
-    #初始化种群
-    X = initialization(pop,ub,lb,dim) 
-    #计算适应度值
-    fitness = CaculateFitness(X,fun) 
-    #对适应度值排序
-    fitness,sortIndex = SortFitness(fitness) 
-    #种群排序
-    X = SortPosition(X,sortIndex) 
+def SMA(pop,dim,lb,ub,MaxIter,fun):
+    #位置更新参数
+    z = 0.03 
+    X = initialization(pop,ub,lb,dim) #初始化种群
+    fitness = CaculateFitness(X,fun) #计算适应度值
+    fitness,sortIndex = SortFitness(fitness) #对适应度值排序
+    X = SortPosition(X,sortIndex) #种群排序
     GbestScore = copy.copy(fitness[0])
     GbestPositon = copy.copy(X[0,:])
     Curve = np.zeros([MaxIter,1])
-
-    #算法参数
-    z = 0.03 
-    #权重W矩阵
-    W = np.zeros([pop,dim]) 
+    W = np.zeros([pop,dim]) #权重W矩阵
     for t in range(MaxIter):
         worstFitness = fitness[-1]
         bestFitness = fitness[0]
-        #当前最优适应度于最差适应度的差值，10E-8为极小值，避免分母为0；
-        S=bestFitness-worstFitness+ 10E-8 
-
-        # 計算權重
+        S=bestFitness-worstFitness+ 10E-8 #当前最优适应度于最差适应度的差值，10E-8为极小值，避免分母为0；
         for i in range(pop):
-
-            #适应度排前一半的W计算
-            if i<pop/2: 
+            if i<pop/2: #适应度排前一半的W计算
                 W[i,:]= 1+np.random.random([1,dim])*np.log10((bestFitness-fitness[i])/(S)+1)
-            #适应度排后一半的W计算
-            else:
+            else:#适应度排后一半的W计算
                 W[i,:]= 1-np.random.random([1,dim])*np.log10((bestFitness-fitness[i])/(S)+1)
-        
         #惯性因子a,b
-        k = 1-(t/MaxIter)
-        if k!=-1 and k!=1:
-            # sigmoid function
-            a = (k+(1-1/(1+np.e**-0.5))*k**2)/(1+k)
+        tt = -(t/MaxIter)+1
+        if tt!=-1 and tt!=1:
+            a = np.math.atanh(tt)
         else:
             a = 1
-        # for vc
         b = 1-t/MaxIter
-
         #位置更新
         for i in range(pop):
             if np.random.random()<z:
-                #全局搜索
-                X[i,:] = (ub.T-lb.T)*np.random.random([1,dim])+lb.T 
+                #公式（1.4）第一个式子
+                # 方法一
+                X[i, :] = (ub.T-lb.T)*np.random.random([1, dim])+lb.T
+                # 方法二
+                # for k in range(dim):
+                #     X[i, :] = (ub[k]-lb[k])*np.random.random([1, dim])+lb[k]
             else:
-
-                # 局部搜索
                 p = np.tanh(abs(fitness[i]-GbestScore))
                 vb = 2*a*np.random.random([1,dim])-a
-                vc = 2*b*np.random.random([1,dim])-2*b
-
-                r1= b
-                r2 = 180*np.random.random()
-                r3 = np.random.random()
-                r4 = np.random.random()
-
+                vc = 2*b*np.random.random([1,dim])-b
                 for j in range(dim):
                     r = np.random.random()
                     A = np.random.randint(pop)
                     B = np.random.randint(pop)
-
                     if r<p:
-                        temp = W[i,j]*X[A,j]-X[B,j]
-                        if r3 < 0.5:
-                            X[i,j] = GbestPositon[j] + r1*np.sin(r2)* vb[0,j]*(temp) 
-                        else:
-                            X[i,j] = GbestPositon[j] + r1*np.cos(r2)* vb[0,j]*(temp) 
-                            
-                    else: 
-                        temp = vc[0,j] * X[i,j]
-                        if r3 < 0.5:
-                            X[i,j] = r1 * np.sin(r2)*temp
-                        else:
-                            X[i,j] = r1 * np.cos(r2)*temp
-
-        #边界检测
-        X = BorderCheck(X,ub,lb,pop,dim) 
-        #计算适应度值
+                        #公式（1.4）第二个式子
+                        X[i,j] = GbestPositon[j] + vb[0,j]*(W[i,j]*X[A,j]-X[B,j]) 
+                        #公式(1.4)第三个式子
+                        X[i,j] = vc[0,j]*X[i,j]         
+        
+        #边界检测 
+        X = BorderCheck(X,ub,lb,pop,dim)
+        #计算适应度值       
         fitness = CaculateFitness(X,fun) 
         #对适应度值排序
         fitness,sortIndex = SortFitness(fitness) 
         #种群排序
         X = SortPosition(X,sortIndex) 
+
         #更新全局最优
         if(fitness[0]<=GbestScore): 
             GbestScore = copy.copy(fitness[0])
@@ -135,6 +108,8 @@ def ESMA(pop,dim,lb,ub,MaxIter,fun):
     
     return GbestScore,GbestPositon,Curve
     # return GbestScore[0], GbestPositon[0][0], Curve
+
+
     
 
 
